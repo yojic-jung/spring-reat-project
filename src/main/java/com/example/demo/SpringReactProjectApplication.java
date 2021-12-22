402262ae46a26625a8451e99bfaa9e105d8ab4ad
yojic-jung <dywlr74@naver.com>
2021-12-23 00:27:54 +0900

    스프링 리액트 첫번째 연동

---
+++ src/main/java/com/example/demo/SpringReactProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactProjectApplication.class, args);
+	}
+
+}
